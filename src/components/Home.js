c3e3cc2de101be22f40eda8fa19ea8e2f27c1d3b
Michael Mathias <michaelscodingjourney@gmail.com>
2023-05-21 20:31:10 -0700

    Added attributes to react components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const color = {color : 'firebrick'};
+  return <div id="home">
+    <h1 style={color}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
